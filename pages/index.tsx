94ee59151f4bd3e0f8c24a7695d5c59bdc0e7c7a
David Dragovacz <david.dragovacz@deliveroo.co.uk>
2022-03-05 01:19:10 +0000

    Remove `Navigation` completely

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,9 @@
-import Navigation from "components/Navigation";
 import Grid from "components/Grid";
 import Footer from "components/Footer";
 
 const Home = () => {
   return (
     <div className="flex h-screen flex-col justify-between">
-      {/* <Navigation /> */}
       <Grid />
       <Footer />
     </div>
