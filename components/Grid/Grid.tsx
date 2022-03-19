9f2e92fcaf9ed2bcdc2bba69fbd68315ac158af0
David Dragovacz <david.dragovacz@deliveroo.co.uk>
2022-03-19 23:35:56 +0000

    Simplify `Grid` layout and motion `Tile`s
    - going for a 3 column layout instead of 4
    - making use of `motion.div`s instead of css classes for the `Tile` component hover/glow (not sure if this sticks)

--- components/Grid/Grid.tsx
+++ components/Grid/Grid.tsx
@@ -3,9 +3,12 @@ import { Responsive, WidthProvider } from "react-grid-layout";
 import { motion } from "framer-motion";
 
 import Tile from "components/Grid/Tile";
+
 import Bio from "components/Bio";
 import Spotify from "components/Spotify";
+import Projects from "components/Projects";
 import Switch from "components/Switch";
+import TBD from "components/TBD";
 import Map from "components/Map";
 import Contact from "components/Contact";
 
@@ -14,15 +17,13 @@ import { GridTile } from "lib/types";
 const ResponsiveGridLayout = WidthProvider(Responsive);
 
 const ROWHEIGHTS: { [key: string]: number } = {
-  xl: 290,
-  lg: 226,
-  md: 204,
-  sm: 164,
+  lg: 283,
+  md: 213,
+  sm: 173,
 };
 
 const BREAKPOINTS = {
-  xl: 1279,
-  lg: 1023,
+  lg: 919,
   md: 479,
   sm: 399,
 };
@@ -31,56 +32,26 @@ const TILES: GridTile[] = [
   {
     id: "bio",
     component: <Bio />,
-    styleOverrides: "lg:hover:cursor-auto",
   },
   {
     id: "spotify",
     component: <Spotify />,
   },
   {
-    id: "fill-one",
-    component: (
-      <div className="grid-tile">
-        <h1 className="gradient-fill-one bg-clip-text text-3xl font-bold text-transparent">
-          This is Fill One
-        </h1>
-      </div>
-    ),
-  },
-  {
-    id: "fill-two",
-    component: (
-      <div className="grid-tile">
-        <h1 className="gradient-fill-two bg-clip-text text-3xl font-bold text-transparent">
-          This is Fill Two
-        </h1>
-      </div>
-    ),
+    id: "projects",
+    component: <Projects />,
   },
   {
     id: "switch",
     component: <Switch />,
   },
   {
-    id: "fill-three",
-    component: (
-      <div className="grid-tile">
-        <h1 className="gradient-fill-three bg-clip-text text-3xl font-bold text-transparent">
-          This is Fill Three
-        </h1>
-      </div>
-    ),
+    id: "map",
+    component: <Map />,
   },
   {
-    id: "map",
-    component: (
-      <div className="grid-tile">
-        <h1 className="gradient-map bg-clip-text text-3xl font-bold text-transparent">
-          This is Map
-        </h1>
-      </div>
-    ),
-    // component: <Map />,
+    id: "tbd",
+    component: <TBD />,
   },
   {
     id: "contact",
@@ -89,43 +60,38 @@ const TILES: GridTile[] = [
 ];
 
 const LAYOUTS = {
-  xl: [
-    { i: "bio", x: 0, y: 0, w: 2, h: 1, static: true },
+  lg: [
+    { i: "bio", x: 0, y: 0, w: 2, h: 1, isResizable: false },
     { i: "spotify", x: 2, y: 0, w: 1, h: 1, isResizable: false },
-    { i: "fill-one", x: 3, y: 0, w: 1, h: 2, isResizable: false },
-    { i: "fill-two", x: 0, y: 1, w: 1, h: 2, isResizable: false },
+    { i: "projects", x: 0, y: 1, w: 1, h: 2, isResizable: false },
     { i: "switch", x: 1, y: 1, w: 1, h: 1, isResizable: false },
-    { i: "fill-three", x: 1, y: 2, w: 1, h: 1, isResizable: false },
     { i: "map", x: 2, y: 1, w: 1, h: 1, isResizable: false },
-    { i: "contact", x: 2, y: 3, w: 2, h: 1, isResizable: false },
+    { i: "tbd", x: 1, y: 2, w: 1, h: 1, isResizable: false },
+    { i: "contact", x: 2, y: 2, w: 1, h: 1, isResizable: false },
   ],
   md: [
-    { i: "bio", x: 0, y: 0, w: 2, h: 1, static: true },
-    { i: "map", x: 0, y: 1, w: 2, h: 1, static: true },
-    { i: "switch", x: 0, y: 2, w: 1, h: 1, static: true },
-    { i: "fill-one", x: 1, y: 2, w: 1, h: 1, static: true },
-    { i: "fill-two", x: 0, y: 3, w: 1, h: 1, static: true },
-    { i: "fill-three", x: 1, y: 3, w: 1, h: 1, static: true },
-    { i: "spotify", x: 0, y: 4, w: 2, h: 1, static: true },
-    { i: "contact", x: 0, y: 5, w: 2, h: 1, static: true },
+    { i: "bio", x: 0, y: 0, w: 2, h: 1.5, static: true },
+    { i: "map", x: 0, y: 1.5, w: 2, h: 1, static: true },
+    { i: "switch", x: 0, y: 2.5, w: 1, h: 1, static: true },
+    { i: "tbd", x: 1, y: 2.5, w: 1, h: 1, static: true },
+    { i: "spotify", x: 0, y: 3.5, w: 2, h: 1, static: true },
+    { i: "contact", x: 0, y: 4.5, w: 2, h: 1, static: true },
+    { i: "projects", x: 0, y: 5.5, w: 2, h: 1.5, static: true },
   ],
   sm: [
     { i: "bio", x: 0, y: 0, w: 2, h: 2, static: true },
-    { i: "map", x: 0, y: 2, w: 2, h: 1, static: true },
-    { i: "switch", x: 0, y: 3, w: 1, h: 1, static: true },
-    { i: "fill-one", x: 1, y: 3, w: 1, h: 1, static: true },
-    { i: "fill-two", x: 0, y: 4, w: 1, h: 1, static: true },
-    { i: "fill-three", x: 1, y: 4, w: 1, h: 1, static: true },
-    { i: "spotify", x: 0, y: 5, w: 2, h: 2, static: true },
-    { i: "contact", x: 0, y: 7, w: 2, h: 2, static: true },
+    { i: "map", x: 0, y: 2, w: 2, h: 1.5, static: true },
+    { i: "switch", x: 0, y: 3.5, w: 1, h: 1, static: true },
+    { i: "tbd", x: 1, y: 3.5, w: 1, h: 1, static: true },
+    { i: "spotify", x: 0, y: 4.5, w: 2, h: 1.5, static: true },
+    { i: "contact", x: 0, y: 6, w: 2, h: 1.5, static: true },
+    { i: "projects", x: 0, y: 7.5, w: 2, h: 1.5, static: true },
   ],
 };
 
 const determineRowHeight = () => {
   if (typeof window !== "undefined") {
-    if (window.innerWidth > 1279) return ROWHEIGHTS.xl;
-    if (window.innerWidth > 1023 && window.innerWidth < 1280)
-      return ROWHEIGHTS.lg;
+    if (window.innerWidth > 1023) return ROWHEIGHTS.lg;
     if (window.innerWidth > 767 && window.innerWidth < 1024)
       return ROWHEIGHTS.md;
     if (window.innerWidth < 768) return ROWHEIGHTS.sm;
@@ -133,18 +99,20 @@ const determineRowHeight = () => {
 };
 
 const Grid = () => {
-  const [rowHeight, setRowHeight] = useState(determineRowHeight);
+  const [rowHeight, setRowHeight] = useState<number | undefined>(
+    determineRowHeight
+  );
 
   return (
     <main className="flex grow items-center justify-center">
-      <div className="container my-16 max-w-[400px] grow md:max-w-[480px] lg:max-w-[1024px] xl:max-w-[1280px]">
+      <div className="w-full max-w-[400px] py-8 md:max-w-[480px] lg:max-w-[920px]">
         <ResponsiveGridLayout
           layouts={LAYOUTS}
           breakpoints={BREAKPOINTS}
-          cols={{ xl: 4, lg: 4, md: 2, sm: 2 }}
+          cols={{ lg: 3, md: 2, sm: 2 }}
           rowHeight={rowHeight}
           isBounded={true}
-          margin={[24, 24]}
+          margin={[18, 18]}
           useCSSTransforms={false}
           onBreakpointChange={(breakpoint: string) =>
             setRowHeight(ROWHEIGHTS[breakpoint])
@@ -154,7 +122,7 @@ const Grid = () => {
           {TILES.map((tile: GridTile, i: number) => (
             <motion.div
               key={tile.id}
-              className={`group transition-all duration-500 ease-out will-change-auto lg:hover:cursor-grab ${tile.styleOverrides}`}
+              className="transition-all duration-700 ease-out will-change-auto lg:cursor-grab"
               initial={{ opacity: 0, scale: 0.5 }}
               animate={{ opacity: 1, scale: 1 }}
               transition={{ delay: 0.1 * i }}
--- components/Grid/Tile.tsx
+++ components/Grid/Tile.tsx
@@ -1,18 +1,56 @@
+import { motion } from "framer-motion";
+
 import Logo from "components/Logo";
 
 import { GridTile } from "lib/types";
+import { gradients } from "lib/gradients";
+
+const glowVariants = {
+  hover: {
+    opacity: 0.7,
+  },
+  initial: {
+    scale: 1.02,
+    opacity: 0,
+  },
+};
+
+const tileVariants = {
+  hover: {
+    scale: 1.02,
+  },
+  initial: {
+    scale: 1,
+  },
+};
 
 const Tile = ({ id, component }: GridTile) => {
   return (
-    <>
-      <div className={`glow gradient-${id}`} />
+    <motion.div initial="initial" whileHover="hover">
+      <motion.div
+        variants={glowVariants}
+        transition={{
+          type: "tween",
+          ease: "easeOut",
+          duration: 0.4,
+        }}
+        className={`glow gradient-${id}`}
+      />
 
-      <div>
-        {id === "bio" && <Logo className="z-10" />}
+      {id === "bio" && <Logo className="z-10" />}
 
+      <motion.div
+        variants={tileVariants}
+        transition={{
+          type: "tween",
+          ease: "easeOut",
+          duration: 0.4,
+        }}
+        className="grid-tile"
+      >
         {component}
-      </div>
-    </>
+      </motion.div>
+    </motion.div>
   );
 };
 
