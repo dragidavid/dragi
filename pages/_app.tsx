af5e72db7813f050d8e047c9eaf2289c1ef09d72
David Dragovacz <david.dragovacz@deliveroo.co.uk>
2022-03-22 19:37:28 +0000

    Remove "logo" svg

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,6 +7,11 @@ import "node_modules/react-resizable/css/styles.css";
 
 import "styles/globals.css";
 
+/**
+ * TODO replace the Logo with something else
+ * TODO find a better way to generate the gradients for the tiles
+ */
+
 const App = ({ Component, pageProps }: AppProps) => {
   return (
     <ThemeProvider attribute="class" defaultTheme="system">
