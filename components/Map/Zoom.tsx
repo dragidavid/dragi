a4a24732d7566fffb866ebc9dd27f8aa112e2c53
David Dragovacz <david.dragovacz@deliveroo.co.uk>
2022-04-10 19:15:51 +0100

    Minor map changes

--- components/Map/Zoom.tsx
+++ components/Map/Zoom.tsx
@@ -8,11 +8,11 @@ const Zoom = () => {
   const { map } = useMap();
 
   return (
-    <div className="absolute right-8 z-[2] h-2/5 w-1 rounded-full opacity-50 transition-opacity duration-200 hover:opacity-100">
+    <div className="absolute right-8 z-[2] h-1/3 w-1 rounded-full opacity-50 transition-opacity duration-200 hover:opacity-100">
       <Root
         aria-label="zoom"
         max={11}
-        min={7}
+        min={5}
         defaultValue={[10]}
         step={0.1}
         className="relative flex h-full w-full touch-none select-none flex-col items-center"
--- components/Map/index.tsx
+++ components/Map/index.tsx
@@ -33,7 +33,7 @@ const Map = () => {
           <Marker latitude={51.5044953} longitude={-0.0098106}>
             <div
               className={classNames(
-                "h-28 w-28 rounded-full bg-gradient-to-br opacity-40 ring-2 ring-[#25B67A]",
+                "h-28 w-28 rounded-full bg-gradient-to-br opacity-30 ring-2 ring-[#25B67A]",
                 GRADIENTS.map.class
               )}
             />
