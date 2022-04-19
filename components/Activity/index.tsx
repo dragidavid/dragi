4a6bb701781e87809add13bd7edf137a4b9c9bf0
David Dragovacz <david.dragovacz@deliveroo.co.uk>
2022-04-19 16:10:55 +0100

    Display error message if fetching fails

--- components/Activity/index.tsx
+++ components/Activity/index.tsx
@@ -1,3 +1,4 @@
+import { useCallback } from "react";
 import useSWR from "swr";
 import { AnimatePresence, motion } from "framer-motion";
 
@@ -10,62 +11,81 @@ import { useLayoutContext } from "contexts/LayoutContext";
 import type { Stats } from "lib/types";
 
 const Activity = () => {
-  const { data: stats } = useSWR<Stats>("/api/github/stats", fetcher, {
+  const { data: stats, error } = useSWR<Stats>("/api/github/stats", fetcher, {
     revalidateOnFocus: false,
   });
 
   const { getInlineGradient } = useLayoutContext();
 
+  const renderContent = useCallback(() => {
+    if (error) {
+      return (
+        <motion.div
+          key="error"
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+          className="text-center text-xs"
+        >
+          Failed to load GitHub stats
+        </motion.div>
+      );
+    }
+
+    if (!stats) {
+      return (
+        <motion.div
+          key="loading"
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+        >
+          <motion.div
+            animate={{ rotate: 360 }}
+            transition={{ repeat: Infinity, duration: 1 }}
+            style={{ backgroundImage: getInlineGradient("activity") }}
+            className="h-[4px] w-8 rounded-[50%]"
+          />
+        </motion.div>
+      );
+    }
+
+    return (
+      <motion.div
+        key="content"
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
+        className="flex h-full w-full flex-col justify-between"
+      >
+        <div className="flex h-full flex-wrap text-center lg:flex-col lg:gap-7">
+          <Stat
+            display={stats.currentStreak.display}
+            value={stats.currentStreak.value}
+          />
+          <Stat
+            display={stats.longestStreak.display}
+            value={stats.longestStreak.value}
+          />
+          <Stat
+            display={stats.contributionsThisYear.display}
+            value={stats.contributionsThisYear.value}
+          />
+          <Stat
+            display={stats.totalContributions.display}
+            value={stats.totalContributions.value}
+          />
+        </div>
+        <p className="text-xs italic">
+          GitHub activity from {new Date(stats.createdAt).toLocaleDateString()}
+        </p>
+      </motion.div>
+    );
+  }, [stats, error]);
+
   return (
     <div className="flex h-full items-center justify-center">
-      <AnimatePresence exitBeforeEnter>
-        {!stats ? (
-          <motion.div
-            key="loading"
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            exit={{ opacity: 0 }}
-          >
-            <motion.div
-              animate={{ rotate: 360 }}
-              transition={{ repeat: Infinity, duration: 1 }}
-              style={{ backgroundImage: getInlineGradient("activity") }}
-              className="h-[4px] w-8 rounded-[50%]"
-            />
-          </motion.div>
-        ) : (
-          <motion.div
-            key="content"
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            exit={{ opacity: 0 }}
-            className="flex h-full w-full flex-col justify-between"
-          >
-            <div className="flex h-full flex-wrap text-center lg:flex-col lg:gap-7">
-              <Stat
-                display={stats.currentStreak.display}
-                value={stats.currentStreak.value}
-              />
-              <Stat
-                display={stats.longestStreak.display}
-                value={stats.longestStreak.value}
-              />
-              <Stat
-                display={stats.contributionsThisYear.display}
-                value={stats.contributionsThisYear.value}
-              />
-              <Stat
-                display={stats.totalContributions.display}
-                value={stats.totalContributions.value}
-              />
-            </div>
-            <p className="text-xs italic">
-              GitHub activity from{" "}
-              {new Date(stats.createdAt).toLocaleDateString()}
-            </p>
-          </motion.div>
-        )}
-      </AnimatePresence>
+      <AnimatePresence exitBeforeEnter>{renderContent()}</AnimatePresence>
     </div>
   );
 };
