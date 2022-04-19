19203c9bd4ca1e7e193af685cae1831a97fc03bb
David Dragovacz <david.dragovacz@deliveroo.co.uk>
2022-04-19 13:50:53 +0100

    Add colors property to `GRADIENT` objects

--- lib/entities.ts
+++ lib/entities.ts
@@ -2,31 +2,38 @@ export const GRADIENTS = {
   bio: {
     class: "from-[#cc208e] to-[#6713d2]",
     inline: "linear-gradient(to bottom right, #cc208e 0%, #6713d2 100%)",
+    colors: ["#cc208e", "#6713d2"],
   },
   spotify: {
     class: "from-[#a9c9ff] to-[#ffbbec]",
     inline: "linear-gradient(to bottom right, #a9c9ff 0%, #ffbbec 100%)",
+    colors: ["#a9c9ff", "#ffbbec"],
   },
   projects: {
     class: "from-[#00dbde] to-[#fc00ff]",
     inline: "linear-gradient(to bottom right, #00dbde 0%, #fc00ff 100%)",
+    colors: ["#00dbde", "#fc00ff"],
   },
   switch: {
     class: "from-[#4158d0] via-[#c850c0] to-[#ffcc70]",
     inline:
       "linear-gradient(to bottom right, #4158d0 0%, #c850c0 50%, #ffcc70 100%)",
+    colors: ["#4158d0", "#c850c0", "#ffcc70"],
   },
   map: {
     class: "from-[#9be15d] to-[#00e3ae]",
     inline: "linear-gradient(to bottom right, #9be15d 0%, #00e3ae 100%)",
+    colors: ["#9be15d", "#00e3ae"],
   },
   activity: {
     class: "from-[#ec4899] via-[#ef4444] to-[#ca8a04]",
     inline:
       "linear-gradient(to bottom right, #ec4899 0%, #ef4444 50%, #ca8a04 100%)",
+    colors: ["#ec4899", "#ef4444", "#ca8a04"],
   },
   contact: {
     class: "from-[#007adf] to-[#00ecbc]",
     inline: "linear-gradient(to bottom right, #007adf 0%, #00ecbc 100%)",
+    colors: ["#007adf", "#00ecbc"],
   },
 };
