ac3eaa91fe864e8235912da5afc5c142a963c16f
David Dragovacz <david.dragovacz@deliveroo.co.uk>
2022-04-25 16:16:12 +0100

    Add links

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,10 +1,64 @@
 import { Html, Head, Main, NextScript } from "next/document";
 
+import classNames from "lib/classNames";
+
 const Document = () => {
   return (
     <Html lang="en">
-      <Head />
-      <body className="bg-white text-gray-700 transition-colors duration-200 ease-in-out dark:bg-black dark:text-gray-300">
+      <Head>
+        <link
+          rel="preload"
+          href="/fonts/inter-var-latin.woff2"
+          as="font"
+          type="font/woff2"
+          crossOrigin="anonymous"
+        />
+        <link href="/static/favicons/favicon.ico" rel="shortcut icon" />
+        <link href="/static/favicons/site.webmanifest" rel="manifest" />
+        <link
+          href="/static/favicons/apple-touch-icon.png"
+          rel="apple-touch-icon"
+          sizes="180x180"
+        />
+        <link
+          href="/static/favicons/favicon-32x32.png"
+          rel="icon"
+          sizes="32x32"
+          type="image/png"
+        />
+        <link
+          href="/static/favicons/favicon-16x16.png"
+          rel="icon"
+          sizes="16x16"
+          type="image/png"
+        />
+        <link
+          color="#4a9885"
+          href="/static/favicons/safari-pinned-tab.svg"
+          rel="mask-icon"
+        />
+        <meta
+          content="/static/favicons/browserconfig.xml"
+          name="msapplication-config"
+        />
+        <meta
+          name="theme-color"
+          content="#ffffff"
+          media="(prefers-color-scheme: light)"
+        />
+        <meta
+          name="theme-color"
+          content="#000000"
+          media="(prefers-color-scheme: dark)"
+        />
+      </Head>
+      <body
+        className={classNames(
+          "bg-white text-gray-700",
+          "dark:bg-black dark:text-gray-300",
+          "transition-colors duration-200 ease-in-out"
+        )}
+      >
         <Main />
         <NextScript />
       </body>
