4c98f70b4ba2271497689a24c41b9dc4eb31d6ff
David Dragovacz <david.dragovacz@deliveroo.co.uk>
2022-05-04 20:12:46 +0100

    Use `DarkModeSwitch` as is

--- components/Switch.tsx
+++ components/Switch.tsx
@@ -1,24 +1,7 @@
-import dynamic from "next/dynamic";
 import { useState, useEffect } from "react";
 import { useTheme } from "next-themes";
 import { useHotkeys } from "react-hotkeys-hook";
 import { DarkModeSwitch } from "react-toggle-dark-mode";
-
-const DynamicDarkModeSwitch = dynamic(
-  () => {
-    const promise = import("react-toggle-dark-mode").then(
-      (module) => module.DarkModeSwitch
-    );
-
-    return promise;
-  },
-  {
-    ssr: false,
-    loading: () => (
-      <DarkModeSwitch checked={false} onChange={() => undefined} />
-    ),
-  }
-);
 
 const Switch = () => {
   const [, setMounted] = useState<boolean>(false);
@@ -39,7 +22,7 @@ const Switch = () => {
 
   return (
     <div className="flex h-full items-center justify-center">
-      <DynamicDarkModeSwitch
+      <DarkModeSwitch
         className="h-12 w-12"
         checked={resolvedTheme === "dark"}
         onChange={handleThemeSwitch}
