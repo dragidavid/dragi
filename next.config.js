49ca3dc8846520aeefafc4038abe409859601c35
David Dragovacz <david.dragovacz@deliveroo.co.uk>
2023-06-11 13:19:33 +0100

    update & remove packages

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
-  swcMinify: true,
+  reactStrictMode: true,
 };
 
 module.exports = nextConfig;
