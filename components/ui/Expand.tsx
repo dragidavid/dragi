8e25d8ae9c03df8580a7512188218ab6c42ca9e4
David Dragovacz <david.dragovacz@deliveroo.co.uk>
2023-07-11 23:00:10 +0100

    add custom, animated Arrow icon

--- components/ui/Expand.tsx
+++ components/ui/Expand.tsx
@@ -1,12 +1,8 @@
-// TODO - Replace current icon with a custom one
-
 "use client";
 
 import { useState, useEffect, useRef } from "react";
 import Link from "next/link";
 import { motion } from "framer-motion";
-
-import { ArrowUpRight } from "lucide-react";
 
 import { cn } from "lib/cn";
 
@@ -63,15 +59,7 @@ export default function Expand({ href }: { href: string }) {
           "group"
         )}
       >
-        <ArrowUpRight
-          size={18}
-          className={cn(
-            "transition-transform duration-75 ease-in-out",
-            "will-change-transform",
-            "group-hover:-translate-y-1 group-hover:translate-x-1 group-hover:scale-105 group-focus:-translate-y-1 group-focus:translate-x-1 group-focus:scale-105"
-          )}
-          aria-hidden={true}
-        />
+        <Arrow />
         <span className="sr-only">Expand</span>
       </Link>
 
@@ -94,6 +82,51 @@ export default function Expand({ href }: { href: string }) {
           />
         </svg>
       )}
+    </div>
+  );
+}
+
+function Arrow() {
+  const variants = {
+    rest: {
+      d: "M2,4 L6,4 L6,8",
+      x2: 6,
+      y2: 4,
+    },
+    hover: {
+      d: "M2,2 L8,2 L8,8",
+      x2: 8,
+      y2: 2,
+      transition: {
+        duration: 0.1,
+      },
+    },
+  };
+
+  return (
+    <div className={cn("h-[18px] w-[18px]", "select-none")}>
+      <motion.svg
+        viewBox="0 0 10 10"
+        width="100%"
+        height="100%"
+        initial="rest"
+        whileHover="hover"
+        animate="rest"
+      >
+        <motion.line
+          x1="1"
+          y1="9"
+          stroke="currentColor"
+          strokeWidth="1"
+          variants={variants}
+        />
+        <motion.path
+          stroke="currentColor"
+          strokeWidth="1"
+          fill="transparent"
+          variants={variants}
+        />
+      </motion.svg>
     </div>
   );
 }
