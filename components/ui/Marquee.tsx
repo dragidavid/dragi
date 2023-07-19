c7685dac32736724fc96037fb7aa7374d987c20e
David Dragovacz <david.dragovacz@deliveroo.co.uk>
2023-07-19 21:23:25 +0100

    make marquee skip tab order

--- components/ui/Marquee.tsx
+++ components/ui/Marquee.tsx
@@ -50,7 +50,6 @@ export default function Marquee({
     return () => {
       window.removeEventListener("resize", getMoveBy);
     };
-    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   useEffect(() => {
@@ -86,6 +85,7 @@ export default function Marquee({
         onHoverStart={startAnimation}
         onTap={startAnimation}
         className={cn("w-max whitespace-nowrap", "will-change-transform")}
+        tabIndex={-1}
       >
         {children}
       </motion.div>
