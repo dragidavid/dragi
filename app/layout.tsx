151814df7916b4107863620045fc7049a5e7ba38
David Dragovacz <david.dragovacz@deliveroo.co.uk>
2023-08-14 18:08:45 +0100

    change main layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -42,10 +42,9 @@ export default function RootLayout({
         <Providers attribute="class" defaultTheme="system" enableSystem>
           <main
             className={cn(
-              "mx-auto w-screen max-w-[448px] overflow-hidden px-5",
-              // "select-none",
+              "mx-auto w-screen max-w-[448px] overflow-hidden",
+              "select-none",
               "xs:flex xs:min-h-screen xs:items-center xs:overflow-y-clip xs:overflow-x-visible",
-              "sm:px-4",
               "md:w-[--container-size] md:max-w-none"
             )}
           >
--- app/page.tsx
+++ app/page.tsx
@@ -116,84 +116,86 @@ const modules = [
 
 export default function Page() {
   return (
-    <div
-      className={cn(
-        "relative flex min-h-screen w-full flex-col items-center justify-start py-12",
-        "xs:justify-center",
-        "md:grid md:h-[--container-size] md:min-h-0 md:grid-cols-3 md:grid-rows-3 md:py-0"
-      )}
-    >
-      <Fade
-        sides={[
-          {
-            id: "top",
-            className: cn(
-              "left-0 top-0 h-[9vh] w-screen",
-              "bg-gradient-to-t from-transparent to-background",
-              "md:h-1/6"
-            ),
-          },
-          {
-            id: "left",
-            className: cn(
-              "top-0 left-0 h-screen w-[7vw] invisible",
-              "bg-gradient-to-l from-transparent to-background",
-              "xs:visible",
-              "sm:w-1/6"
-            ),
-          },
-          {
-            id: "bottom",
-            className: cn(
-              "bottom-0 left-0 h-[11vh] w-screen",
-              "bg-gradient-to-b from-transparent to-background",
-              "md:h-1/6"
-            ),
-          },
-          {
-            id: "right",
-            className: cn(
-              "top-0 right-0 h-screen w-[7vw] invisible",
-              "bg-gradient-to-r from-transparent to-background",
-              "xs:visible",
-              "sm:w-1/6"
-            ),
-          },
-        ]}
-      />
-
-      <Line
+    <div className={cn("h-full w-full px-5", "sm:px-4")}>
+      <div
         className={cn(
-          "-bottom-full -top-full left-0 w-px",
-          "-translate-x-1/2",
-          "md:hidden"
+          "relative flex min-h-screen w-full flex-col items-center justify-start py-12",
+          "xs:justify-center",
+          "md:grid md:h-[--container-size] md:min-h-0 md:grid-cols-3 md:grid-rows-3 md:py-0"
         )}
-      />
+      >
+        <Fade
+          sides={[
+            {
+              id: "top",
+              className: cn(
+                "left-0 top-0 h-[9vh] w-screen",
+                "bg-gradient-to-t from-transparent to-background",
+                "md:h-1/6"
+              ),
+            },
+            {
+              id: "left",
+              className: cn(
+                "top-0 left-0 h-screen w-[7vw] invisible",
+                "bg-gradient-to-l from-transparent to-background",
+                "xs:visible",
+                "sm:w-1/6"
+              ),
+            },
+            {
+              id: "bottom",
+              className: cn(
+                "bottom-0 left-0 h-[11vh] w-screen",
+                "bg-gradient-to-b from-transparent to-background",
+                "md:h-1/6"
+              ),
+            },
+            {
+              id: "right",
+              className: cn(
+                "top-0 right-0 h-screen w-[7vw] invisible",
+                "bg-gradient-to-r from-transparent to-background",
+                "xs:visible",
+                "sm:w-1/6"
+              ),
+            },
+          ]}
+        />
 
-      {modules.map((module) => (
-        <Module
-          key={module.id}
-          id={module.id}
-          page={module.page}
-          preview
-          lines={module.lines}
-          joints={module.joints}
-          moduleStyles={cn(
-            "flex h-full max-h-[calc(var(--container-size)*2/3)]",
-            module.moduleStyles
+        <Line
+          className={cn(
+            "-bottom-full -top-full left-0 w-px",
+            "-translate-x-1/2",
+            "md:hidden"
           )}
-        >
-          {module.component}
-        </Module>
-      ))}
+        />
 
-      <Line
-        className={cn(
-          "-bottom-full -top-full right-0 w-px",
-          "translate-x-1/2",
-          "md:hidden"
-        )}
-      />
+        {modules.map((module) => (
+          <Module
+            key={module.id}
+            id={module.id}
+            page={module.page}
+            preview
+            lines={module.lines}
+            joints={module.joints}
+            className={cn(
+              "flex h-full max-h-[calc(var(--container-size)*2/3)]",
+              module.moduleStyles
+            )}
+          >
+            {module.component}
+          </Module>
+        ))}
+
+        <Line
+          className={cn(
+            "-bottom-full -top-full right-0 w-px",
+            "translate-x-1/2",
+            "md:hidden"
+          )}
+        />
+      </div>
     </div>
   );
 }
