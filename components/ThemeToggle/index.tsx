2517862cf4deaee7e33d064afb6f7b659dd3e29d
David Dragovacz <david.dragovacz@deliveroo.co.uk>
2023-08-17 00:25:40 +0100

    simple theme toggle

--- components/ThemeToggle/index.tsx
+++ components/ThemeToggle/index.tsx
@@ -19,28 +19,17 @@ export default function ThemeToggle() {
   }
 
   return (
-    <div className={cn("relative pl-px pr-3")}>
-      <button
-        onClick={() => setTheme(theme === "light" ? "dark" : "light")}
-        className={cn(
-          "outline-none",
-          "text-secondary",
-          "transition-colors duration-100 ease-in-out",
-          "hover:text-primary",
-          "focus:text-primary"
-        )}
-      >
-        <span>{theme === "light" ? "dark" : "light"}</span>
-
-        <span className="sr-only">Toggle theme</span>
-      </button>
-
-      <div
-        className={cn(
-          "absolute -right-8 top-0 z-10 h-full w-8",
-          "bg-gradient-to-r from-background via-background/50 to-transparent"
-        )}
-      />
-    </div>
+    <button
+      onClick={() => setTheme(theme === "light" ? "dark" : "light")}
+      className={cn(
+        "outline-none",
+        "transition-colors duration-100 ease-in-out",
+        "hover:text-primary",
+        "focus:text-primary"
+      )}
+    >
+      <span>{theme === "light" ? "dark" : "light"}</span>
+      <span className="sr-only">Toggle theme</span>
+    </button>
   );
 }
