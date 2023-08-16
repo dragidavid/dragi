0ed1eb1a85bc7ed99b6d991bdf7a45211e58375c
David Dragovacz <david.dragovacz@deliveroo.co.uk>
2023-08-17 00:25:23 +0100

    remove hex pattern

--- components/ui/Pattern.tsx
+++ components/ui/Pattern.tsx
@@ -57,33 +57,6 @@ const patterns = {
       />
     </>
   ),
-  hex: (
-    <>
-      <defs>
-        <pattern
-          id="hex-pattern"
-          height="50.19"
-          width="29"
-          patternUnits="userSpaceOnUse"
-          patternTransform="scale(0.5) rotate(0)"
-        >
-          <rect x="0" y="0" height="100%" width="100%" fill="none" />
-          <path
-            d="M14.498 16.858L0 8.488.002-8.257l14.5-8.374L29-8.26l-.002 16.745zm0 50.06L0 58.548l.002-16.745 14.5-8.373L29 41.8l-.002 16.744zM28.996 41.8l-14.498-8.37.002-16.744L29 8.312l14.498 8.37-.002 16.745zm-29 0l-14.498-8.37.002-16.744L0 8.312l14.498 8.37-.002 16.745z"
-            fill="none"
-            stroke="currentColor"
-            strokeWidth="1"
-          />
-        </pattern>
-      </defs>
-      <rect
-        height="100%"
-        width="100%"
-        fill="url(#hex-pattern)"
-        transform="translate(0,0)"
-      />
-    </>
-  ),
   squiggle: (
     <>
       <defs>
@@ -177,7 +150,12 @@ export default memo(function Pattern({
 }) {
   return (
     <div
-      className={cn("h-full w-full", className)}
+      className={cn(
+        "h-full w-full",
+        "pointer-events-none",
+        "text-line/90",
+        className
+      )}
       style={{
         WebkitMaskImage: `linear-gradient(to bottom, transparent, hsl(var(--background)))`,
       }}
