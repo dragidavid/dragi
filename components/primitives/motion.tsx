4bf08ca26d0c0effbc5321704e97eb6827397440
David Dragovacz <david.dragovacz@deliveroo.co.uk>
2023-11-20 21:41:35 +0000

    make motion ok on server

---
+++ components/primitives/motion.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+import { motion } from "framer-motion";
+
+export const MotionDiv = motion.div;
+export const MotionSpan = motion.span;
+export const MotionSvg = motion.svg;
+export const MotionLine = motion.line;
+export const MotionPath = motion.path;
