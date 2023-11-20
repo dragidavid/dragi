ce87b5406c7ba58a7bbe19b3c29799833aa9f09e
David Dragovacz <david.dragovacz@deliveroo.co.uk>
2023-11-20 21:36:44 +0000

    experimental ppr

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@ const { withContentlayer } = require("next-contentlayer");
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    ppr: true,
+  },
   reactStrictMode: true,
   swcMinify: true,
   images: {
