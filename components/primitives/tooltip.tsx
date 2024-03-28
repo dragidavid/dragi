c914ef0fd3ecba78d0a57566eb41704f98330d06
David Dragovacz <dragovacz.dd@gmail.com>
2024-03-28 15:23:33 +0000

    remove unused components

--- components/primitives/tooltip.tsx
+++ components/primitives/tooltip.tsx
@@ -19,7 +19,7 @@ const TooltipContent = React.forwardRef<
     ref={ref}
     sideOffset={sideOffset}
     className={cn(
-      "z-[100] overflow-hidden rounded-md px-3 py-1.5 text-xs",
+      "z-50 min-w-72 overflow-hidden rounded-md text-xs",
       "border border-accent bg-extreme shadow-lg",
       "animate-in fade-in-0 zoom-in-95",
       "data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=closed]:zoom-out-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2",
