07df4990a8e7fb3ca7f7e6c4026a428557dca13b
David Dragovacz <dragovacz.dd@gmail.com>
2024-03-28 15:22:35 +0000

    remove atoms

--- contexts/Providers.tsx
+++ contexts/Providers.tsx
@@ -1,17 +1,14 @@
 "use client";
 
 import { TooltipProvider } from "components/primitives/tooltip";
-import { Provider as JotaiProvider } from "jotai";
-import { ThemeProvider as NextThemesProvider } from "next-themes";
+import { ThemeProvider } from "next-themes";
 
 import { type ThemeProviderProps } from "next-themes/dist/types";
 
 export default function Providers({ children, ...props }: ThemeProviderProps) {
   return (
-    <NextThemesProvider {...props}>
-      <JotaiProvider>
-        <TooltipProvider>{children}</TooltipProvider>
-      </JotaiProvider>
-    </NextThemesProvider>
+    <ThemeProvider {...props}>
+      <TooltipProvider delayDuration={100}>{children}</TooltipProvider>
+    </ThemeProvider>
   );
 }
