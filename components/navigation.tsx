58ea4c2bc5565783ceb1d4adee597d8d165af2f2
David Dragovacz <dragovacz.dd@gmail.com>
2024-08-31 15:20:16 +0100

    navigation change

--- components/navigation.tsx
+++ components/navigation.tsx
@@ -1,13 +1,14 @@
 "use client";
 
+import { useCallback, useEffect, useRef } from "react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import { AnimatePresence } from "framer-motion";
+import { AnimatePresence, useScroll, useSpring } from "framer-motion";
 
 import Line from "components/line";
 import Cross from "components/cross";
 
-import { MotionSpan } from "components/primitives/motion";
+import { MotionNav, MotionSpan } from "components/primitives/motion";
 
 import { cn } from "lib/cn";
 
@@ -34,17 +35,78 @@ const links = [
   },
 ] as const;
 
+const useScrollDirection = (pathname: string) => {
+  const lastScrollY = useRef(0);
+  const isAtEdge = useRef(false);
+
+  const { scrollY } = useScroll();
+
+  const scale = useSpring(1, {
+    stiffness: 100,
+    damping: 30,
+  });
+
+  useEffect(() => {
+    if (scale.get() !== 1) {
+      scale.set(1);
+    }
+  }, [pathname]);
+
+  useEffect(() => {
+    return scrollY.on("change", (latest) => {
+      const scrollDelta = latest - lastScrollY.current;
+      const isScrollingDown = scrollDelta > 0;
+      const isScrollingUp = scrollDelta < 0;
+
+      const isAtTop = latest <= 0;
+      const isAtBottom =
+        latest + window.innerHeight >= document.documentElement.scrollHeight;
+
+      if ((isAtTop && isScrollingUp) || (isAtBottom && isScrollingDown)) {
+        isAtEdge.current = true;
+        return;
+      }
+
+      if (isAtEdge.current && !isAtTop && !isAtBottom) {
+        isAtEdge.current = false;
+        return;
+      }
+
+      if (!isAtEdge.current) {
+        if (isScrollingDown) {
+          scale.set(0.6);
+        } else if (isScrollingUp) {
+          scale.set(1);
+        }
+      }
+
+      lastScrollY.current = latest;
+    });
+  }, [scrollY, scale]);
+
+  return scale;
+};
+
 export default function Navigation() {
   const pathname = usePathname();
 
-  const isActive = (href: string) => pathname.includes(href) && href !== "/";
+  const scale = useScrollDirection(pathname);
 
+  const isActive = useCallback(
+    (href: string) => pathname.includes(href) && href !== "/",
+    [pathname],
+  );
   return (
-    <nav
+    <MotionNav
+      style={{ scale, transformOrigin: "bottom center" }}
       className={cn(
-        "relative flex h-auto w-full font-mono text-2xs",
-        "text-secondary",
-        "xs:h-full",
+        "relative flex h-auto w-full overflow-hidden rounded-3xl font-mono text-2xs",
+        "bg-gradient-to-b text-secondary backdrop-blur-md",
+        "from-extreme to-inverse/[0.02]",
+        "shadow-[0px_0px_0px_1px,0px_1px_1px_-0.5px,0px_3px_3px_-1.5px,0px_6px_6px_-3px,0px_12px_12px_-6px,0px_24px_24px_-12px] shadow-inverse/[0.04]",
+        "dark:from-inverse/[0.01] dark:to-extreme/20",
+        "dark:shadow-[inset_0px_1px_0px_0px_hsla(var(--inverse)/0.05),inset_0px_0px_1px_0px_hsla(var(--inverse)/0.08),0_0_1px_hsla(var(--extreme)/0.3),0_2px_2px_hsla(var(--extreme)/0.3),0_4px_4px_hsla(var(--extreme)/0.3),0_8px_8px_hsla(var(--extreme)/0.3)]",
+        "xs:h-full xs:overflow-visible xs:rounded-none xs:bg-transparent xs:!shadow-none xs:backdrop-blur-none xs:![background-image:none]",
         "md:text-xs",
       )}
     >
@@ -54,11 +116,12 @@ export default function Navigation() {
           href={href}
           title={label}
           className={cn(
-            "relative flex-1",
+            "relative aspect-square w-full flex-1",
             "outline-none",
             "hover:text-primary",
             "focus:text-primary",
             "focus-visible:after:absolute focus-visible:after:bottom-0 focus-visible:after:left-0 focus-visible:after:z-50 focus-visible:after:h-px focus-visible:after:w-full focus-visible:after:translate-y-1/2 focus-visible:after:bg-primary",
+            "xs:aspect-auto",
             isActive(href) && "text-primary",
           )}
         >
@@ -105,7 +168,9 @@ export default function Navigation() {
                   isActive(href) && "text-primary",
                 )}
                 style={{
-                  WebkitTextStroke: "2px hsl(var(--muted))",
+                  WebkitTextStroke: isActive(href)
+                    ? "1px hsla(var(--inverse)/0.04)"
+                    : "1px hsl(var(--muted))",
                 }}
               >
                 {index}
@@ -131,7 +196,7 @@ export default function Navigation() {
                   className={cn(
                     "absolute top-0 size-full",
                     "pointer-events-none",
-                    "bg-gradient-to-b from-primary/5 to-transparent",
+                    "bg-gradient-to-b from-muted/80 to-transparent",
                   )}
                 />
               )}
@@ -139,6 +204,6 @@ export default function Navigation() {
           </div>
         </Link>
       ))}
-    </nav>
+    </MotionNav>
   );
 }
