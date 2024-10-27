578fb093e09db6e09b7fb12c7e34616fcd325292
David Dragovacz <dragovacz.dd@gmail.com>
2024-10-27 20:40:19 +0000

    lines

--- components/line.tsx
+++ components/line.tsx
@@ -1,6 +1,7 @@
 import { cn } from "lib/cn";
 
 import type { Corner } from "lib/types";
+import { MotionDiv } from "./primitives/motion";
 
 const cornerToOrigin: Record<Corner, string> = {
   tl: "top left",
@@ -21,19 +22,26 @@ export default function Line({
   length?: number;
   origin?: Corner;
 }) {
-  const tiltedLineStyle: React.CSSProperties = {
-    position: "absolute",
-    width: `${length}px`,
-    height: "1px",
-    transformOrigin: origin ? cornerToOrigin[origin] : undefined,
-    transform: `rotate(${angle}deg)`,
+  const baseProps = {
+    className: cn("absolute z-30", "pointer-events-none", className),
+    style: tilted
+      ? {
+          height: "1px",
+          transformOrigin: origin ? cornerToOrigin[origin] : undefined,
+          transform: `rotate(${angle}deg)`,
+        }
+      : {},
+    "aria-hidden": true,
   };
 
-  return (
-    <div
-      className={cn("absolute z-30", "pointer-events-none", className)}
-      style={tilted ? tiltedLineStyle : {}}
-      aria-hidden
+  return tilted ? (
+    <MotionDiv
+      {...baseProps}
+      initial={{ width: 0 }}
+      animate={{ width: `${length}px` }}
+      transition={{ duration: 0.8 }}
     />
+  ) : (
+    <div {...baseProps} />
   );
 }
