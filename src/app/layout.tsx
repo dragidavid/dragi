62142b0cc4f5283a0bd216cb5a43e67490689a21
David Dragovacz <dragovacz.dd@gmail.com>
2025-06-01 16:49:57 +0100

    adjust layout + add metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,43 @@
+import { Analytics } from "@vercel/analytics/react";
+
 import { GeistSans } from "geist/font/sans";
-import { GeistMono } from "geist/font/mono";
+import { Instrument_Serif } from "next/font/google";
 
-import Providers from "@/contexts/Providers";
-
-import Frame from "@/components/frame";
+import { Providers } from "@/contexts/Providers";
 
 import { cn } from "@/lib/cn";
 
 import "@/app/globals.css";
+
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  metadataBase: new URL("https://drgd.fyi"),
+  title: "David Dragovacz",
+  description: "Product engineer working at Remote.",
+  openGraph: {
+    title: "David Dragovacz",
+    description: "Product engineer working at Remote.",
+    images: ["/og.png"],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "David Dragovacz",
+    description: "Product engineer working at Remote.",
+    images: ["/og.png"],
+  },
+  icons: {
+    icon: "/favicon.ico",
+    shortcut: "/favicon-32x32.png",
+    apple: "/apple-touch-icon.png",
+  },
+};
+
+const instrumentSerif = Instrument_Serif({
+  subsets: ["latin"],
+  variable: "--font-instrument-serif",
+  weight: ["400"],
+});
 
 export default function RootLayout({
   children,
@@ -17,30 +47,45 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className={cn(GeistSans.variable, GeistMono.variable, "antialiased")}
+      dir="ltr"
+      className={cn(
+        GeistSans.variable,
+        instrumentSerif.variable,
+        "antialiased",
+      )}
       suppressHydrationWarning
     >
-      <head />
       <body
         className={cn(
-          "overflow-x-clip",
+          "touch-pan-y",
           "bg-background text-primary caret-primary",
-          "selection:bg-rose-100 selection:text-rose-900",
+          "selection:bg-rose-100 selection:text-rose-600",
           "dark:selection:bg-rose-400/10 dark:selection:text-rose-400",
         )}
       >
         <Providers attribute="class">
-          <Frame />
-
           <main
             className={cn(
-              "isolate mx-auto w-screen max-w-md overflow-hidden",
-              "xs:flex xs:min-h-screen xs:items-center xs:overflow-y-clip xs:overflow-x-visible",
-              "md:w-(--container-size) md:max-w-none",
+              "isolate mx-auto h-dvh w-full max-w-screen",
+              "border-inverse/20 border-dashed",
+              "md:flex md:w-(--container-size) md:max-w-none md:items-center md:border-x",
             )}
           >
+            {/* Horizontal borders across */}
+            <div
+              aria-hidden
+              className={cn(
+                "absolute inset-x-0 hidden h-[calc(var(--container-size)+2px)]",
+                "pointer-events-none",
+                "border-inverse/20 border-y border-dashed",
+                "md:block",
+              )}
+            />
+
             {children}
           </main>
+
+          <Analytics />
         </Providers>
       </body>
     </html>
