7a1c2ef0e34437a4f50193460c7f862c6cb50c48
David Dragovacz <david.dragovacz@remote.com>
2025-06-04 21:42:46 +0100

    change styles

--- src/components/motion-cross.tsx
+++ src/components/motion-cross.tsx
@@ -1,4 +1,7 @@
+"use client";
+
 import { memo } from "react";
+import { motion } from "motion/react";
 
 import { cn } from "@/lib/cn";
 
@@ -8,12 +11,12 @@ const CORNER_STYLES: Record<Corner, React.CSSProperties> = {
   tl: {
     top: 0,
     left: 0,
-    transform: "translate(-45%, -50%)",
+    transform: "translate(-45%, -55%)",
   },
   tr: {
     top: 0,
     right: 0,
-    transform: "translate(50%, -50%)",
+    transform: "translate(45%, -55%)",
   },
   bl: {
     bottom: 0,
@@ -23,7 +26,7 @@ const CORNER_STYLES: Record<Corner, React.CSSProperties> = {
   br: {
     bottom: 0,
     right: 0,
-    transform: "translate(50%, 55%)",
+    transform: "translate(45%, 55%)",
   },
 } as const;
 
@@ -36,25 +39,32 @@ export const Cross = memo(({ positions }: { positions: Corner[] }) => {
         const delay = Math.random() * (0.8 - 0) + 0;
 
         return (
-          <div
+          <motion.div
             key={position}
+            initial={{
+              opacity: 0,
+            }}
+            animate={{
+              opacity: 1,
+              filter: "brightness(60%)",
+            }}
+            transition={{
+              opacity: { duration: 0.2, delay },
+              filter: { duration: 0.4, delay: delay + 0.2 },
+            }}
             aria-hidden
             className={cn(
-              "absolute z-10 hidden size-4",
+              "absolute z-10 hidden size-[13px]",
               "pointer-events-none",
               "md:block",
             )}
             style={CORNER_STYLES[position]}
           >
             <div className={cn("relative grid size-full place-items-center")}>
-              <div
-                className={cn(
-                  "relative size-[11px] rounded-sm",
-                  "bg-inverse/20 border-background border backdrop-blur-sm",
-                )}
-              />
+              <span className={cn("absolute h-px w-full", "bg-inverse")} />
+              <span className={cn("absolute h-full w-px", "bg-inverse")} />
             </div>
-          </div>
+          </motion.div>
         );
       })}
     </>
